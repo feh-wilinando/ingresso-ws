de2c7ad884704062e94da8d70df944bb80cfd2a3
Fernando Furtado <feh.wilinando@gmail.com>
2017-01-02 20:23:14 -0200

    Gambis

--- src/main/java/br/com/caelum/ingresso_ws/Application.java
+++ src/main/java/br/com/caelum/ingresso_ws/Application.java
@@ -1,8 +1,12 @@
 package br.com.caelum.ingresso_ws;
 
+import br.com.caelum.ingresso_ws.controller.FilmesController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by nando on 02/01/17.
@@ -14,4 +18,14 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+    @Autowired
+    private FilmesController filmesController;
+
+    @PostConstruct
+    public void onResume(){
+        filmesController.getFilmes();
+    }
+
 }
